3ee60927f3fdafdc4b9975271e9324e8e406f7bb
tungcbh <ntt.a5k62.cbh@gmail.com>
2025-04-11 23:44:02 +0700

    feat: add implement for get basket and store basket

--- eshopms/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ eshopms/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,31 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿using FluentValidation;
+
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(bool IsSuccess);
+    public class StoreBasketValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasketValidator()
+        {
+            RuleFor(x => x.Cart)
+                .NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName)
+                .NotEmpty().WithMessage("Username is required");
+        }
+        public class StoreBasketCommandHandler
+            : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+        {
+            public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+            {
+                ShoppingCart cart = command.Cart;
+
+                //store cart in db - use Marten upsert
+
+                //update cache
+
+                return new StoreBasketResult(true);
+            }
+        }
     }
 }
---
+++ eshopms/src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using Basket.API.Models;
+
+
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Behaviors;
+
+global using Carter;
+global using MediatR;
+global using Mapster;
