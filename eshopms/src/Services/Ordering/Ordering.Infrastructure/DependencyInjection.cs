e8e9e3b1d513c2f95749055379860ef2c921bf62
tungcbh <ntt.a5k62.cbh@gmail.com>
2025-04-13 03:27:59 +0700

    feat: add DI for layers

---
+++ eshopms/src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            // Add services to the container
+            //services.AddDbContext<ApplicationDbContext>(option =>
+            //    option.UseSqlServer(connectionString));
+
+            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
