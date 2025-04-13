e8747d6af538195133b7a5d8305f3e62c6c6dd19
tungcbh <ntt.a5k62.cbh@gmail.com>
2025-04-14 04:05:35 +0700

    feat: change to strongly typed IDs

--- eshopms/src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ eshopms/src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -1,8 +1,22 @@
 ﻿namespace Ordering.Domain.Models
 {
-    public class Customer
+    public class Customer : Entity<CustomerId>
     {
         public string Name { get; private set; } = default!;
         public string Email { get; private set; } = default!;
+        public static Customer Create(CustomerId customerId, string name, string email)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name, nameof(name));
+            ArgumentException.ThrowIfNullOrWhiteSpace(email, nameof(email));
+
+            var customer = new Customer
+            {
+                Id = customerId,
+                Name = name,
+                Email = email
+            };
+
+            return customer;
+        }
     }
 }
---
+++ eshopms/src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record OrderName
+    {
+        public string Value { get; }
+
+    }
+}
