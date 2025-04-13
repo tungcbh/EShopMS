0453a2eb25b503be5dc239239ac179564eee4a5f
tungcbh <ntt.a5k62.cbh@gmail.com>
2025-04-14 03:46:42 +0700

    feat: init models, value objects and enums

---
+++ eshopms/src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Customer
+    {
+        public string Name { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+    }
+}
